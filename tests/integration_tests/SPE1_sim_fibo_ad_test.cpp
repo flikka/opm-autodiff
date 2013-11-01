99880ec06d12d05df8545f6a6ace7e1b053f0d66
Kristian Flikka <kristian.flikka@gmail.com>
2013-11-01 15:19:00 +0100

    Some commenting in data file and test. Temp work

--- tests/integration_tests/SPE1_sim_fibo_ad_test.cpp
+++ tests/integration_tests/SPE1_sim_fibo_ad_test.cpp
@@ -52,6 +52,11 @@
 #include <opm/autodiff/SimulatorFullyImplicitBlackoil.hpp>
 #include <opm/autodiff/BlackoilPropsAdFromDeck.hpp>
 
+#include <opm/parser/eclipse/Parser/Parser.hpp>
+#include <opm/parser/eclipse/EclipseState/Schedule/Schedule.hpp>
+#include <opm/parser/eclipse/Deck/Deck.hpp>
+
+
 #include <boost/scoped_ptr.hpp>
 #include <boost/filesystem.hpp>
 
@@ -62,6 +67,114 @@
 
 
 using namespace Opm;
+
+std::vector<BlackoilState> runWithNewParser(parameter::ParameterGroup param) {
+    
+    ParserPtr parser(new Parser());
+    DeckConstPtr deck = parser->parse("SPE1_opm.DATA");
+    ScheduleConstPtr schedule_deck(new Schedule(deck));
+    
+    std::cout << "Heisann! \n ";
+    boost::scoped_ptr<EclipseGridParser> old_deck;
+    boost::scoped_ptr<GridManager> grid;
+    boost::scoped_ptr<BlackoilPropertiesInterface> props;
+    boost::scoped_ptr<BlackoilPropsAdInterface> new_props;
+    boost::scoped_ptr<RockCompressibility> rock_comp;
+    BlackoilState state;
+    std::vector<BlackoilState> state_collection;
+
+    double gravity[3] = {0.0};
+    std::string deck_filename = "SPE1_opm.DATA";
+    old_deck.reset(new EclipseGridParser(deck_filename));
+    // Grid init
+    grid.reset(new GridManager(*old_deck));
+    // Rock and fluid init
+    props.reset(new BlackoilPropertiesFromDeck(*old_deck, *grid->c_grid(), param));
+    new_props.reset(new BlackoilPropsAdFromDeck(*old_deck, *grid->c_grid()));
+
+    rock_comp.reset(new RockCompressibility(*old_deck));
+
+    gravity[2] = old_deck->hasField("NOGRAV") ? 0.0 : unit::gravity;
+
+    initBlackoilStateFromDeck(*grid->c_grid(), *props, *old_deck, gravity[2], state);
+
+    bool use_gravity = (gravity[0] != 0.0 || gravity[1] != 0.0 || gravity[2] != 0.0);
+    const double *grav = use_gravity ? &gravity[0] : 0;
+
+    // Boundary conditions.
+    FlowBCManager bcs;
+
+    // Linear solver.
+    LinearSolverFactory linsolver(param);
+
+    std::cout << "\n\n================    Starting main simulation loop     ===============\n"
+            << "                        (number of epochs: "
+            << (old_deck->numberOfEpochs()) << ")\n\n" << std::flush;
+
+    SimulatorReport rep;
+    // With a deck, we may have more epochs etc.
+    WellState well_state;
+    int step = 0;
+    SimulatorTimer simtimer;
+    // Use timer for last epoch to obtain total time.
+    old_deck->setCurrentEpoch(old_deck->numberOfEpochs() - 1);
+    simtimer.init(*old_deck);
+    const double total_time = simtimer.totalTime();
+    for (int epoch = 0; epoch < old_deck->numberOfEpochs(); ++epoch) {
+        // Set epoch index.
+        old_deck->setCurrentEpoch(epoch);
+
+        // Update the timer.
+        if (old_deck->hasField("TSTEP")) {
+            simtimer.init(*old_deck);
+        } else {
+            if (epoch != 0) {
+                OPM_THROW(std::runtime_error, "No TSTEP in deck for epoch " << epoch);
+            }
+            simtimer.init(param);
+        }
+        simtimer.setCurrentStepNum(step);
+        simtimer.setTotalTime(total_time);
+
+        // Report on start of epoch.
+        std::cout << "\n\n--------------    Starting epoch " << epoch << "    --------------"
+                << "\n                  (number of steps: "
+                << simtimer.numSteps() - step << ")\n\n" << std::flush;
+
+        // Create new wells, well_state
+        WellsManager wells(*old_deck, *grid->c_grid(), props->permeability());
+        // @@@ HACK: we should really make a new well state and
+        // properly transfer old well state to it every epoch,
+        // since number of wells may change etc.
+        if (epoch == 0) {
+            well_state.init(wells.c_wells(), state);
+        }
+
+        // Create and run simulator.
+        SimulatorFullyImplicitBlackoil simulator(param,
+                *grid->c_grid(),
+                *new_props,
+                rock_comp->isActive() ? rock_comp.get() : 0,
+                wells,
+                bcs.c_bcs(),
+                linsolver,
+                grav);
+     
+        SimulatorReport epoch_rep = simulator.run(simtimer, state, well_state);
+        
+        BlackoilState copy = state;
+        state_collection.push_back(copy);
+        // Update total timing report and remember step number.
+        rep += epoch_rep;
+        step = simtimer.currentStepNum();
+    }
+
+    std::cout << "\n\n================    End of simulation     ===============\n\n"; 
+    rep.report(std::cout);
+    
+    return state_collection;
+}
+
 
 std::vector<BlackoilState> runWithOldParser(parameter::ParameterGroup param) {
     boost::scoped_ptr<EclipseGridParser> deck;
@@ -165,118 +278,21 @@ std::vector<BlackoilState> runWithOldParser(parameter::ParameterGroup param) {
 }
 
 
-std::vector<BlackoilState> runWithNewParser(parameter::ParameterGroup param) {
-    boost::scoped_ptr<EclipseGridParser> deck;
-    boost::scoped_ptr<GridManager> grid;
-    boost::scoped_ptr<BlackoilPropertiesInterface> props;
-    boost::scoped_ptr<BlackoilPropsAdInterface> new_props;
-    boost::scoped_ptr<RockCompressibility> rock_comp;
-    BlackoilState state;
-    std::vector<BlackoilState> state_collection;
-
-    double gravity[3] = {0.0};
-    std::string deck_filename = "SPE1_opm.DATA";
-    deck.reset(new EclipseGridParser(deck_filename));
-    // Grid init
-    grid.reset(new GridManager(*deck));
-    // Rock and fluid init
-    props.reset(new BlackoilPropertiesFromDeck(*deck, *grid->c_grid(), param));
-    new_props.reset(new BlackoilPropsAdFromDeck(*deck, *grid->c_grid()));
-
-    rock_comp.reset(new RockCompressibility(*deck));
-
-    gravity[2] = deck->hasField("NOGRAV") ? 0.0 : unit::gravity;
-
-    initBlackoilStateFromDeck(*grid->c_grid(), *props, *deck, gravity[2], state);
-
-    bool use_gravity = (gravity[0] != 0.0 || gravity[1] != 0.0 || gravity[2] != 0.0);
-    const double *grav = use_gravity ? &gravity[0] : 0;
-
-    // Boundary conditions.
-    FlowBCManager bcs;
-
-    // Linear solver.
-    LinearSolverFactory linsolver(param);
-
-    std::cout << "\n\n================    Starting main simulation loop     ===============\n"
-            << "                        (number of epochs: "
-            << (deck->numberOfEpochs()) << ")\n\n" << std::flush;
-
-    SimulatorReport rep;
-    // With a deck, we may have more epochs etc.
-    WellState well_state;
-    int step = 0;
-    SimulatorTimer simtimer;
-    // Use timer for last epoch to obtain total time.
-    deck->setCurrentEpoch(deck->numberOfEpochs() - 1);
-    simtimer.init(*deck);
-    const double total_time = simtimer.totalTime();
-    for (int epoch = 0; epoch < deck->numberOfEpochs(); ++epoch) {
-        // Set epoch index.
-        deck->setCurrentEpoch(epoch);
-
-        // Update the timer.
-        if (deck->hasField("TSTEP")) {
-            simtimer.init(*deck);
-        } else {
-            if (epoch != 0) {
-                OPM_THROW(std::runtime_error, "No TSTEP in deck for epoch " << epoch);
-            }
-            simtimer.init(param);
-        }
-        simtimer.setCurrentStepNum(step);
-        simtimer.setTotalTime(total_time);
-
-        // Report on start of epoch.
-        std::cout << "\n\n--------------    Starting epoch " << epoch << "    --------------"
-                << "\n                  (number of steps: "
-                << simtimer.numSteps() - step << ")\n\n" << std::flush;
-
-        // Create new wells, well_state
-        WellsManager wells(*deck, *grid->c_grid(), props->permeability());
-        // @@@ HACK: we should really make a new well state and
-        // properly transfer old well state to it every epoch,
-        // since number of wells may change etc.
-        if (epoch == 0) {
-            well_state.init(wells.c_wells(), state);
-        }
-
-        // Create and run simulator.
-        SimulatorFullyImplicitBlackoil simulator(param,
-                *grid->c_grid(),
-                *new_props,
-                rock_comp->isActive() ? rock_comp.get() : 0,
-                wells,
-                bcs.c_bcs(),
-                linsolver,
-                grav);
-     
-        SimulatorReport epoch_rep = simulator.run(simtimer, state, well_state);
-        
-        BlackoilState copy = state;
-        state_collection.push_back(copy);
-        // Update total timing report and remember step number.
-        rep += epoch_rep;
-        step = simtimer.currentStepNum();
-    }
-
-    std::cout << "\n\n================    End of simulation     ===============\n\n"; 
-    rep.report(std::cout);
-    
-    return state_collection;
-}
-
 
 BOOST_AUTO_TEST_CASE(SPE1_runWithOldAndNewParser_BlackOilStateEqual) {
-    int argc = 2;
-    const char ** argv = (const char **)malloc(2* sizeof(argv));
-    argv[1] = "spe1.xml";
-    parameter::ParameterGroup param(argc, argv, false);
+    Parser * parser = new Parser();
+    DeckConstPtr deck = parser->parse("SPE1_opm.DATA", true);
+//    ScheduleConstPtr schedule_deck(new Schedule(deck));
+    //int argc = 2;
+    //const char ** argv = (const char **)malloc(2* sizeof(argv));
+    //argv[1] = "spe1.xml";
+    //parameter::ParameterGroup param(argc, argv, false);
     
-    std::vector<BlackoilState> runWithOldParserStates = runWithOldParser(param);
-    std::vector<BlackoilState> runWithNewParserStates = runWithNewParser(param);
-    
-    for(size_t i=0; i<runWithOldParserStates.size(); i++) {
-        BOOST_CHECK(runWithOldParserStates[i].equals(runWithNewParserStates[i]));
-    }
+    //std::vector<BlackoilState> runWithOldParserStates = runWithOldParser(param);
+    //std::vector<BlackoilState> runWithNewParserStates = runWithNewParser(param);
+//    
+//    for(size_t i=0; i<runWithOldParserStates.size(); i++) {
+//        BOOST_CHECK(runWithOldParserStates[i].equals(runWithNewParserStates[i]));
+//    }
 }
+
